0a2ce3e84a528c5ff7daf31f6dff16e0b8987e4a
Carlo Villaceran <cvillaceran@gmail.com>
2019-05-10 19:59:34 -0700

    add user model for sequelize

--- server/models/user.js
+++ server/models/user.js
@@ -1,84 +1,12 @@
-const mongoose = require('mongoose')
-const validator = require('validator')
-const _ = require('lodash')
-const bcrypt = require('bcrypt')
-
-//declare schema to tack on custom methods
-const UserSchema = new mongoose.Schema({
-  displayName: {
-    type: String,
-    required: true,
-    minLength: 1,
-    trim: true
-  },
-  email: {
-    type: String,
-    required: true,
-    minLength: 1,
-    trim: true,
-    unique: true,
-    validate: {
-      validator: validator.isEmail,
-      message: '{VALUE} is not a valid email'
-    }
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 5
-  },
-  checkedInTo: { type: mongoose.Schema.Types.ObjectId }
-})
-
-//override toJSON method
-UserSchema.methods.toJSON = function() {
-  const user = this
-  const userObject = user.toObject()
-
-  return _.pick(userObject, ['_id', 'email', 'checkedInTo'])
-}
-
-UserSchema.methods.removeToken = function(token) {
-  const user = this
-
-  return user.update({
-    $pull: { tokens: { token } }
-  })
-}
-
-UserSchema.statics.findByCredentials = function(email, password) {
-  const User = this
-
-  return User.findOne({ email }).then(user => {
-    if (!user) return Promise.reject()
-
-    return new Promise((resolve, reject) => {
-      bcrypt.compare(password, user.password, (err, match) => {
-        if (match) {
-          return resolve(user)
-        } else {
-          return reject('401')
-        }
-      })
-    })
-  })
-}
-
-UserSchema.pre('save', function(next) {
-  const user = this
-
-  if (user.isModified('password')) {
-    bcrypt.genSalt(10, (err, salt) => {
-      bcrypt.hash(user.password, salt, (err, hash) => {
-        user.password = hash
-        next()
-      })
-    })
-  } else {
-    next()
-  }
-})
-
-const User = mongoose.model('User', UserSchema)
-
-module.exports = User
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING
+  }, {});
+  User.associate = function(models) {
+    // associations can be defined here
+  };
+  return User;
+};
